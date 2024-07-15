6c7d985360a6d0b3a1f8b2f1c0a3acbe730d5dce
Vadim Sergeev <vadevrazserg@gmail.com>
2024-07-15 09:56:34 +0700

    featch data from database

--- dashboard/starter-example/app/lib/data.ts
+++ dashboard/starter-example/app/lib/data.ts
@@ -24,7 +24,21 @@ export async function fetchRevenue() {
     return data.rows;
   } catch (error) {
     console.error('Database Error:', error);
-    throw new Error('Failed to fetch revenue data.');
+    // throw new Error('Failed to fetch revenue data.');
+    return [
+      { month: 'Jan', revenue: 2000 },
+      { month: 'Feb', revenue: 1800 },
+      { month: 'Mar', revenue: 2200 },
+      { month: 'Apr', revenue: 2500 },
+      { month: 'May', revenue: 2300 },
+      { month: 'Jun', revenue: 3200 },
+      { month: 'Jul', revenue: 3500 },
+      { month: 'Aug', revenue: 3700 },
+      { month: 'Sep', revenue: 2500 },
+      { month: 'Oct', revenue: 2800 },
+      { month: 'Nov', revenue: 3000 },
+      { month: 'Dec', revenue: 4800 },
+    ]
   }
 }
 
@@ -41,10 +55,48 @@ export async function fetchLatestInvoices() {
       ...invoice,
       amount: formatCurrency(invoice.amount),
     }));
+
     return latestInvoices;
   } catch (error) {
     console.error('Database Error:', error);
-    throw new Error('Failed to fetch the latest invoices.');
+    // throw new Error('Failed to fetch the latest invoices.');
+    return [
+      {
+        amount: '$448.00',
+        name: 'Michael Novotny',
+        image_url: '/customers/michael-novotny.png',
+        email: 'michael@novotny.com',
+        id: 'dd95af9a-af0d-4467-863c-5883a9ec6a3b'
+      },
+      {
+        amount: '$5.00',
+        name: 'Delba de Oliveira',
+        image_url: '/customers/delba-de-oliveira.png',
+        email: 'delba@oliveira.com',
+        id: 'e0ced172-790b-4491-a584-55d4a81c14f0'
+      },
+      {
+        amount: '$345.77',
+        name: 'Balazs Orban',
+        image_url: '/customers/balazs-orban.png',
+        email: 'balazs@orban.com',
+        id: '27c4659a-ef59-494b-8394-0a76657f2717'
+      },
+      {
+        amount: '$542.46',
+        name: 'Lee Robinson',
+        image_url: '/customers/lee-robinson.png',
+        email: 'lee@robinson.com',
+        id: '601dde96-2a86-4f1c-b046-78380b05796c'
+      },
+      {
+        amount: '$6.66',
+        name: 'Evil Rabbit',
+        image_url: '/customers/evil-rabbit.png',
+        email: 'evil@rabbit.com',
+        id: '7a151efe-d092-40ea-8c95-ba840626df72'
+      }
+    ]
   }
 }
 
@@ -79,7 +131,13 @@ export async function fetchCardData() {
     };
   } catch (error) {
     console.error('Database Error:', error);
-    throw new Error('Failed to fetch card data.');
+    // throw new Error('Failed to fetch card data.');
+    return {
+      numberOfInvoices: 13,
+      numberOfCustomers: 6,
+      totalPaidInvoices: '$1,006.26',
+      totalPendingInvoices: '$1,256.32',
+    }
   }
 }
 
